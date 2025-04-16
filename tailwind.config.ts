2cce7d5a0ecabc9476829f43beeebd5659847ce8
ha-quang.le <winlp4ever@gmail.com>
2025-04-16 22:41:42 +0000

    build(*): update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
 			fontFamily: {
 				sans: ['var(--font-dm-sans)'],
 				mono: ['var(--font-dm-mono)'],
-				handwriting: ['var(--font-indie-flower)'],
+				handwriting: ['var(--font-tillana)'],
 			},
   		colors: {
   			background: 'hsl(var(--background))',
